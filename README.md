11b9ad46b42182c410b76d7e033ec16e37fe9b12
Eugene Dementyev <eugene@springload.co.nz>
2021-04-14 14:55:05 +1200

    Add README and define types for variables for clarity

---
+++ README.md
@@ -0,0 +1,5 @@
+# terraform-aws-ecs-service
+
+This module creates an ECS service. It supports load balanced services if required.
+
+Has additional security measures that can be optionally enabled, such as RO filesystem, or "run as specified user".
--- variables.tf
+++ variables.tf
@@ -1,50 +1,59 @@
 variable "cluster_name" {
   description = "Name of the ECS cluster"
+  type        = string
 }
 
 variable "service_name" {
   description = "Name of the ECS service"
+  type        = string
 }
 
 variable "target_group_name" {
   description = "Name of the ALB target group. Defaults to the `cluster_name` if not set"
+  type        = string
   default     = ""
 }
 
 variable "target_group_names" {
   description = "Names of the ALB target groups. Defaults to the `[cluster_name]` if not set"
+  type        = list(string)
   default     = []
 }
 
 
 variable "environment" {
   description = "Enviropnment vars to pass to the container. Note: they will be visible in the task definition, so please don't pass any secrets here."
-  type        = map
+  type        = map(any)
   default     = {}
 
 }
 
 variable "image" {
-  description = "The image to use"
+  description = "The Docker image to use"
+  type        = string
 }
 
 variable "cpu" {
   description = "Amount of CPU to use"
+  type        = number
   default     = 0
 }
 
 variable "essential" {
   description = "Exit the task if contaner exits"
+  type        = bool
   default     = true
 }
 
 variable "memory" {
   description = "Amount of maximum memory the container can use"
+  type        = number
   default     = 512
 }
 
 variable "memory_reservation" {
   description = "Amount of reserved memory for the container"
+  type        = number
   default     = 256
 }
 
@@ -70,7 +79,7 @@ variable "task_role_arn" {
 }
 
 variable "port_mappings" {
-  type    = list
+  type    = list(any)
   default = []
 }
 
@@ -104,14 +113,14 @@ variable "desired_count" {
 }
 
 variable "init_process_enabled" {
-  type        = bool
   description = "Use embdedded to Docker tini init process that correctly reaps zombie processes"
+  type        = bool
   default     = true
 }
 
 variable "user" {
-  type        = string
   description = "Run container as the specified user. Formats are: user, user:group, uid, uid:gid, user:gid, uid:group"
+  type        = string
   default     = ""
 }
 
