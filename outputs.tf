db359ef2091f77d93906f9cbb914c0ff7bb32bd3
Andrei Vsiakikh <andrei.vsiakikh@springload.co.nz>
2024-03-21 12:16:26 +1300

    output for fargate

--- outputs.tf
+++ outputs.tf
@@ -6,3 +6,8 @@ output "task-definition-family" {
   description = "Family of the task definition"
   value       = aws_ecs_task_definition.task.family
 }
+
+# that could be usefull fot the fargate later
+output "task_execution_role_arn" {
+  value = aws_iam_role.task-execution-role.arn
+}
