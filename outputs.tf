2ca1b8301e86910fd9596b020c1629a47fabef06
Andrei Vsiakikh <andrei.vsiakikh@springload.co.nz>
2025-11-24 13:15:36 +1300

    linting

--- outputs.tf
+++ outputs.tf
@@ -8,13 +8,16 @@ output "task-definition-family" {
 }
 
 output "task_execution_role_arn" {
-  value = aws_iam_role.task-execution-role.arn
+  description = "ARN of the task execution role used by the ECS service"
+  value       = aws_iam_role.task-execution-role.arn
 }
 
 output "fargate_spot" {
-  value = var.fargate_spot
+  description = "Whether Fargate Spot launch type is enabled"
+  value       = var.fargate_spot
 }
 
 output "fargate" {
-  value = var.fargate
+  description = "Whether Fargate launch type is enabled"
+  value       = var.fargate
 }
